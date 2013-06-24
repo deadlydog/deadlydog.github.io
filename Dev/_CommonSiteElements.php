362202473a8367b215bbd917d9e86223fa4f6940
deadlydog <deadlydog@hotmail.com>
2013-06-24 11:35:07 -0600

    - Removed Websites I like tab.
    - Updated blog address.

--- Dev/_CommonSiteElements.php
+++ Dev/_CommonSiteElements.php
@@ -7,7 +7,7 @@
         <li><a href="GamesIveMade.php" <?php if ($SelectedTab == "Games"){print("class='SelectedTab'");}?>>Games I've Made</a></li>
         <li><a href="AppsIveMade.php" <?php if ($SelectedTab == "Apps"){print("class='SelectedTab'");}?>>Apps I've Made</a></li>
         <li><a href="WebsitesIveMade.php" <?php if ($SelectedTab == "Websites"){print("class='SelectedTab'");}?>>Websites I've Made</a></li>
-        <li><a href="WebsitesILike.php" <?php if ($SelectedTab == "WebsitesILike"){print("class='SelectedTab'");}?>>Websites I Like</a></li>
+        <!-- <li><a href="WebsitesILike.php" <?php if ($SelectedTab == "WebsitesILike"){print("class='SelectedTab'");}?>>Websites I Like</a></li> -->
         <li><a href="index.php" <?php if ($SelectedTab == "AboutMe"){print("class='SelectedTab'");}?>>About Me</a></li>
 	</ul>
 </div>
--- Dev/index.php
+++ Dev/index.php
@@ -12,14 +12,13 @@
 <div id="Content">
 <h1>Welcome to Daniel Schroeder's Website</h1>
 
-<p>I live in Regina, Saskatchewan, Canada with my wonderful wife Vanessa Schroeder and our two bunnies, Liddle and Daisy. 
-I completed my Masters degree in Computer Science at the University of Regina in 2009, and am now the Team Leader for the SDET team 
-(Software Developer Engineer in Test) at iQmetrix. 
+<p>I live in Regina, Saskatchewan, Canada with my wonderful wife Vanessa Schroeder, our son Noah, our dog Molly, and our bunny Liddle. 
+I completed my Masters degree in Computer Science at the University of Regina in 2009, and am now the Team Lead for the DevOps team at iQmetrix. 
 I enjoy creating video games and websites, and have a passion for programming.</p>
 
 <p>If you want to get in contact with me you can <a href="mailto:deadlydog@hotmail.com?subject=DansKingdom.com">email me</a></p>
 
-<p>You can also <a href="http://geekswithblogs.net/deadlydog/Default.aspx">check out my blog</a> or <a href="http://www.twitter.com/deadlydog">follow me on twitter</a>.</p>
+<p>You can also <a href="http://blog.danskingdom.com">check out my blog</a> or <a href="http://www.twitter.com/deadlydog">message me on twitter</a>.</p>
 
 <p>If you are looking for Marvin Schroeder's pictures go to <a href="http://marvinspictures.danskingdom.com">http://MarvinsPictures.danskingdom.com</a></p>
 <p>If you are looking for Jeremy Schroeder's stuff <a href="Jeremy/index.php">go here</a>
