b145fe1f45f2b63842b861ae56fe1a09b1810b66
Daniel Schroeder <deadlydog@hotmail.com>
2019-03-21 00:29:37 -0600

    - Delete all content except the Prd directory. - Renamed 'Prd' to 'src'.

--- DPSF/Prd/_HTMLHead.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="google-site-verification" content="iVVhFy-3WjTqgog4o-gnPjYerOP3yi8hjhZIeQzNY_c" /> <!-- Google Webmaster Tools Verify Link -->
-<link rel="alternate" type="application/rss+xml"  href="http://dpsf.danskingdom.com/RSS.xml" title="DPSF RSS Feed">
-<link rel="stylesheet" type="text/css" href="/CSS/DPSF.css" />
-<script type="text/javascript" src="/Javascript/DPSF.js"></script>
-
--- Prd/Jeremy/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>Jeremy's Stuff</title>
-</head>
-
-<body>
-<a href="Taiwan Movie.wmv">Taiwan Movie.wmv</a>
-</body>
-</html>
