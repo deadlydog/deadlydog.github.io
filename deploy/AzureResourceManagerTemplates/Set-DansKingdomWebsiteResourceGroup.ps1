e9cb25b1349159c565effb74b95091660bb0ad88
deadlydog <deadlydog@hotmail.com>
2020-04-05 11:24:53 -0600

    chore: Add script for deploying ARM template

---
+++ deploy/AzureResourceManagerTemplates/Set-DansKingdomWebsiteResourceGroup.ps1
@@ -0,0 +1,8 @@
+$resourceGroupName = 'DansKingdomWebsite'
+$location = 'eastus2'
+
+[string] $THIS_SCRIPTS_DIRECTORY_PATH = $PSScriptRoot
+[string] $sharedResourcesArmTemplateFilePath = Join-Path -Path $THIS_SCRIPTS_DIRECTORY_PATH -ChildPath 'DansKingdomWebsite-ResourceGroup\template.json'
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $sharedResourcesArmTemplateFilePath -Mode Incremental -Verbose
