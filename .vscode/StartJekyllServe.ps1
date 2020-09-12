b8069c211db1c4259f4fb13a47e4a0e1b3e563a5
deadlydog <deadlydog@hotmail.com>
2020-09-12 11:54:53 -0600

    chore: Update script logging a bit

--- .vscode/StartJekyllServe.ps1
+++ .vscode/StartJekyllServe.ps1
@@ -1,5 +1,5 @@
 param([string] $projectDirectoryPath)
 
 Set-Location -Path $projectDirectoryPath
-Write-Host "Starting blog at http://127.0.0.1:4000 by running 'bundle exec jekyll serve'. It may take a minute to start..."
+Write-Host "Starting blog at http://127.0.0.1:4000 by running 'bundle exec jekyll serve --draft'. It may take a minute to start..."
 & bundle exec jekyll serve --draft
