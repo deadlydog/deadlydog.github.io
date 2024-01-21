1f73a21f908baeeecbdc0894010a1957ed0dd3f7
deadlydog <deadlydog@hotmail.com>
2024-01-21 17:26:03 -0600

    fix: Only call Post Advertisement function if it is defined

--- _includes/CodeToRunWhenCookiesAreAccepted.js
+++ _includes/CodeToRunWhenCookiesAreAccepted.js
@@ -1,5 +1,12 @@
 // Call the functions that should not be ran until the user has accepted cookies.
-RunGoogleAdsensePostAdvertisementJavaScript()
+
+// If the user is not viewing a post, the Post Ad function will not be defined, so only call it if it is defined.
+if (typeof(RunGoogleAdsensePostAdvertisementJavaScript) === typeof(Function))
+{
+	console.log("RunGoogleAdsensePostAdvertisementJavaScript is defined.");
+	RunGoogleAdsensePostAdvertisementJavaScript()
+}
+
 RunGoogleAdsenseFooterAdvertisementJavaScript()
 RunGoogleAnalyticsJavaScript()
 RunMsClarityJavaScript()
