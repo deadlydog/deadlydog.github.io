8b5d1c9706b08150cb4fb9ec533e86920372da3e
deadlydog <deadlydog@hotmail.com>
2024-02-17 18:50:36 -0600

    experiment: See if Google Adsense is GDPR compliant

--- _includes/CodeToRunWhenCookiesAreAccepted.js
+++ _includes/CodeToRunWhenCookiesAreAccepted.js
@@ -1,16 +1,16 @@
 // Call the functions that should not be ran until the user has accepted cookies.
 
-// If the user is not viewing a post, the Post Ad function will not be defined, so only call it if it is defined.
-if (typeof(RunGoogleAdsensePostAdvertisementJavaScript) === typeof(Function))
-{
-	RunGoogleAdsensePostAdvertisementJavaScript()
-}
+// // If the user is not viewing a post, the Post Ad function will not be defined, so only call it if it is defined.
+// if (typeof(RunGoogleAdsensePostAdvertisementJavaScript) === typeof(Function))
+// {
+// 	RunGoogleAdsensePostAdvertisementJavaScript()
+// }
 
-RunGoogleAdsenseFooterAdvertisementJavaScript()
+// RunGoogleAdsenseFooterAdvertisementJavaScript()
 RunMsClarityJavaScript()
 
-// // Google Analytics are not included when running locally/debugging, so to avoid an error only call this function if it is defined.
-// if (typeof (RunGoogleAdsensePostAdvertisementJavaScript) === typeof(Function))
-// {
-// 	RunGoogleAnalyticsJavaScript()
-// }
+// Google Analytics are not included when running locally/debugging, so to avoid an error only call this function if it is defined.
+if (typeof (RunGoogleAdsensePostAdvertisementJavaScript) === typeof(Function))
+{
+	RunGoogleAnalyticsJavaScript()
+}
